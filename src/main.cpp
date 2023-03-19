6b9a9ed054eba317d7f543c16d4bc187a50f5e73
ntdat <dat10hoa1@gmail.com>
2023-03-19 18:20:11 +0700

    4 pumps

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,20 @@
 
 #define _____MACRO_DEFINE_____
 
-#define LED_PIN LED_BUILTIN
+#define LED_POWER LED_BUILTIN
 #define WIFI_SSID "VNPT T2"
 #define WIFI_PASSWORD "18061996"
-#define PUMP_00 D2
-#define PUMP_01 D5
-#define PUMP_02 D6
+#define PUMP_0 D8
+#define PUMP_1 D7
+#define PUMP_2 D6
+#define PUMP_3 D5
+
+#define LED_0 D4
+#define LED_1 D3
+#define LED_2 D2
+#define LED_3 D1
+
+#define ANALOG_PIN A0
 
 #define MQTT_BROKER_DOMAIN 	"broker.emqx.io" 
 #define MQTT_BROKER_PORT		(1883U)
@@ -22,12 +30,18 @@
 #define MQTT_USERNAME "esp8266"
 #define MQTT_USERPASSWORD "123456"
 
-// GPIO_Custom testLed(LED_PIN, (GPIO_MODE_e)eGPIO_MODE_OUTPUT, (GPIO_STATE_e)eGPIO_STATE_LOW);
 MQTT_custom pumpClient;
-GPIO_Custom ledState = GPIO_Custom(LED_PIN, (GPIO_MODE_e)eGPIO_MODE_OUTPUT);
-GPIO_Custom pump00x = GPIO_Custom(PUMP_00, (GPIO_MODE_e)eGPIO_MODE_INPUT);
-GPIO_Custom pump01x = GPIO_Custom(PUMP_01, (GPIO_MODE_e)eGPIO_MODE_INPUT);
-GPIO_Custom pump02x = GPIO_Custom(PUMP_02, (GPIO_MODE_e)eGPIO_MODE_INPUT);
+
+GPIO_Custom pump0x = GPIO_Custom(PUMP_0, (GPIO_MODE_e)eGPIO_MODE_INPUT);
+GPIO_Custom pump1x = GPIO_Custom(PUMP_1, (GPIO_MODE_e)eGPIO_MODE_INPUT);
+GPIO_Custom pump2x = GPIO_Custom(PUMP_2, (GPIO_MODE_e)eGPIO_MODE_INPUT);
+GPIO_Custom pump3x = GPIO_Custom(PUMP_3, (GPIO_MODE_e)eGPIO_MODE_INPUT);
+
+// turn off by default
+GPIO_Custom led0x = GPIO_Custom(LED_0, (GPIO_MODE_e)eGPIO_MODE_OUTPUT);
+GPIO_Custom led1x = GPIO_Custom(LED_1, (GPIO_MODE_e)eGPIO_MODE_OUTPUT);
+GPIO_Custom led2x = GPIO_Custom(LED_2, (GPIO_MODE_e)eGPIO_MODE_OUTPUT);
+GPIO_Custom led3x = GPIO_Custom(LED_3, (GPIO_MODE_e)eGPIO_MODE_OUTPUT);
 
 // void setup_wifi(void);
 void callback(char*, byte*, unsigned int);
@@ -35,8 +49,22 @@ void callback(char*, byte*, unsigned int);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial.setDebugOutput(true);
 	delay(2000);
-	pinMode(LED_PIN, OUTPUT);
+
+
+	// blink to indicate starting
+	led0x.setState((GPIO_STATE_e)eGPIO_STATE_HIGH);
+	led1x.setState((GPIO_STATE_e)eGPIO_STATE_HIGH);
+	led2x.setState((GPIO_STATE_e)eGPIO_STATE_HIGH);
+	led3x.setState((GPIO_STATE_e)eGPIO_STATE_HIGH);
+	delay(200);
+	led0x.setState((GPIO_STATE_e)eGPIO_STATE_LOW);
+	led1x.setState((GPIO_STATE_e)eGPIO_STATE_LOW);
+	led2x.setState((GPIO_STATE_e)eGPIO_STATE_LOW);
+	led3x.setState((GPIO_STATE_e)eGPIO_STATE_LOW);
+
+	// end blink
 
 	pumpClient.connectWifi(WIFI_SSID, WIFI_PASSWORD);
 	pumpClient.setupMQTT(MQTT_BROKER_DOMAIN, MQTT_BROKER_PORT);
@@ -57,86 +85,86 @@ void callback(char* topic, byte* payload, unsigned int length)
 
 	pumpClient.setCurrentSubMessage(tempMessage);
 
-	Serial.printf("Message arrived [%s]: %s\n\n", pumpClient.getMQTTTopicSub(), pumpClient.getCurrentSubMessage());
+	Serial.printf("Message arrived [%s]: %s\n", pumpClient.getMQTTTopicSub(), pumpClient.getCurrentSubMessage());
 
-	if(strncmp(tempMessage, "led off", sizeof(tempMessage)) == 0)
-	{
-		digitalWrite(LED_PIN, 1);
-	}
-	else	if(strncmp(tempMessage, "led on", sizeof(tempMessage)) == 0)
-	{
-		digitalWrite(LED_PIN, 0);
-	}
-	else
-	{
-		// do nothing
-	}
 }
 
-// void reconnect() {
-//   // Chờ tới khi kết nối
-//   while (!client.connected()) {
-//     Serial.print("Attempting MQTT connection...");
-//     // Thực hiện kết nối với mqtt user và pass
-//     if (client.connect("ESP8266Client")) 
-// 		{
-//       Serial.println("connected");
-//       // Khi kết nối sẽ publish thông báo
-//       client.publish(MQTT_TOPIC_PUB, "test OK");
-//       // ... và nhận lại thông tin này
-//       client.subscribe(MQTT_TOPIC_SUB);
-//     } 
-// 		else
-// 		{
-//       Serial.print("failed, rc=");
-//       Serial.print(client.state());
-//       Serial.println(" try again in 5 seconds");
-//       // Đợi 5s
-//       delay(5000);
-//     }
-// 		yield();
-//   }
-// }
-
 void loop() {
-  // if (!client.connected()) {
-  //   reconnect();
-  // }
-  // client.loop();
-
-	// value++;
-	// snprintf (msg, (uint8_t)sizeof(msg), "hello world #%d", value);
-	// Serial.print("Publish message: ");
-	// Serial.println(msg);
-	// client.publish(MQTT_TOPIC_SUB, msg);
-
-	// yield();
-
-	// delay(2000);
 
 	char myString[100];
 
-	ledState.setState((GPIO_STATE_e)eGPIO_STATE_LOW);
 	pumpClient.keepListenFromMQTTBroker();
 
 
 	Serial.println("===== STATE OF PUMPS =====");
 	memset(myString, 0, 100);
-	sprintf(myString, "pump_A_00_%d", (uint8_t)pump00x.getState());
+	sprintf(myString, "pump_A_00_%d", (uint8_t)pump0x.getState());
 	pumpClient.publishMessage(myString);
+	if(pump0x.getState() == (GPIO_STATE_e)eGPIO_STATE_HIGH)
+	{
+		// turn on
+		led0x.setState((GPIO_STATE_e)eGPIO_STATE_HIGH);
+		// digitalWrite(LED_0, 1);
+	}
+	else
+	{
+		// turn off
+		led0x.setState((GPIO_STATE_e)eGPIO_STATE_LOW);
+		// digitalWrite(LED_0, 0);
+	}
 	Serial.println(pumpClient.getCurrentPubMessage());
+
+	delay(15000);
 
 	memset(myString, 0, 100);
-	sprintf(myString, "pump_A_01_%d", (uint8_t)pump01x.getState());
+	sprintf(myString, "pump_A_01_%d", (uint8_t)pump1x.getState());
 	pumpClient.publishMessage(myString);
+	if(pump1x.getState() == (GPIO_STATE_e)eGPIO_STATE_HIGH)
+	{
+		// turn on
+		led1x.setState((GPIO_STATE_e)eGPIO_STATE_HIGH);
+	}
+	else
+	{
+		// turn off
+		led1x.setState((GPIO_STATE_e)eGPIO_STATE_LOW);
+	}
 	Serial.println(pumpClient.getCurrentPubMessage());
+
+	delay(15000);
 
 	memset(myString, 0, 100);
-	sprintf(myString, "pump_A_02_%d", (uint8_t)pump02x.getState());
+	sprintf(myString, "pump_A_02_%d", (uint8_t)pump2x.getState());
 	pumpClient.publishMessage(myString);
+	if(pump2x.getState() == (GPIO_STATE_e)eGPIO_STATE_HIGH)
+	{
+		// turn on
+		led2x.setState((GPIO_STATE_e)eGPIO_STATE_HIGH);
+	}
+	else
+	{
+		// turn off
+		led2x.setState((GPIO_STATE_e)eGPIO_STATE_LOW);
+	}
 	Serial.println(pumpClient.getCurrentPubMessage());
 
-	Serial.println("===========================\n\n\n\n\n");
-	delay(2000);
+	delay(15000);
+
+	memset(myString, 0, 100);
+	sprintf(myString, "pump_A_03_%d", (uint8_t)pump3x.getState());
+	pumpClient.publishMessage(myString);
+	if(pump3x.getState() == (GPIO_STATE_e)eGPIO_STATE_HIGH)
+	{
+		// turn on
+		led3x.setState((GPIO_STATE_e)eGPIO_STATE_HIGH);
+	}
+	else
+	{
+		// turn off
+		led3x.setState((GPIO_STATE_e)eGPIO_STATE_LOW);
+	}
+	Serial.println(pumpClient.getCurrentPubMessage());
+
+	delay(15000);
 
 }
